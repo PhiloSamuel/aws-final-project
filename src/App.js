b6d884d353edf6de753a2f718fc1c460620abd1f
PhiloSamuel <samuelphilopater@yahoo.com>
2024-09-30 22:48:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const teamMembers = [
+    // { name: 'Philopateer', imageUrl: 'https://webprojectpics.s3.amazonaws.com/philooo.png' },
+    { name: 'Philo', imageUrl: 'https://webprojectpics.s3.amazonaws.com/philooo.png' },
+    { name: 'Belal', imageUrl: '' },
+    { name: 'Mostafa', imageUrl: '' },
+    { name: 'Marwan', imageUrl: '' },
+    { name: 'Moatasem', imageUrl: '' },
+
+
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>AWS Final Project</h1>
       </header>
+
+      <section className="team-members">
+        <h2>Team Members</h2>
+        <div className="card-container">
+          {teamMembers.map((member, index) => (
+            <div className="card" key={index}>
+              <img src={member.imageUrl} alt={`${member.name}'s avatar`} />
+              <h3>{member.name}</h3>
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
 }
